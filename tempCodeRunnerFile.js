166b03d72b8bc4fc3c52b58fa717c9fdd0b549c7
TatendaLatmore <159183456+TatendaLatmore@users.noreply.github.com>
2024-04-04 13:52:37 -0400

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+//Task 1
+console.log("apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010");
+
+console.log("\n\n")
+
+//Task 2
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080\n\n");
+
+//task 3
+console.log("x\to\tx");
+console.log("o\tx\to");
+console.log("x\to\tx");
